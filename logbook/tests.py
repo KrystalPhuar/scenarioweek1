7cf33a8fd4a0c242190c1003768b3a4cd6bc96d5
Ying Lin <zcablin@ucl.ac.uk>
2017-11-03 14:23:50 +0000

    test

--- logbook/tests.py
+++ logbook/tests.py
@@ -15,9 +15,10 @@ class SignupTest(LiveServerTestCase):
 		super(SignupTest, self).tearDown()
 
 	def test_login(self):
+		print('Signup')
 		driver = self.driver
 		driver.get('http://127.0.0.1:8000/logbook/signup/')
-		driver.find_element_by_id("id_username").send_keys("temp@gmail.com")
+		driver.find_element_by_id("id_username").send_keys("temptest@gmail.com")
 		driver.find_element_by_id("id_password1").send_keys("ucl393939")
 		driver.find_element_by_id("id_password2").send_keys("ucl393939")
 		driver.find_element_by_css_selector("button[type=submit]").click()
@@ -33,9 +34,10 @@ class LoginTest(LiveServerTestCase):
 		self.driver.quit()
 		super(LoginTest, self).tearDown()
 	def test_login(self):
+		print('login')
 		driver = self.driver
 		driver.get('http://127.0.0.1:8000/logbook/')
-		driver.find_element_by_id("id_username").send_keys("temp@gmail.com")
+		driver.find_element_by_id("id_username").send_keys("temp22@gmail.com")
 		driver.find_element_by_id("id_password").send_keys("ucl393939")
 		driver.find_element_by_css_selector("input[type=submit]").click()
 		time.sleep(3)
@@ -51,9 +53,10 @@ class Testadd(LiveServerTestCase):
 		self.driver.quit()
 		super(Testadd, self).tearDown()
 	def test_login(self):
+		print('add')
 		driver = self.driver
 		driver.get('http://127.0.0.1:8000/logbook/')
-		driver.find_element_by_id("id_username").send_keys("temp@gmail.com")
+		driver.find_element_by_id("id_username").send_keys("temp22@gmail.com")
 		driver.find_element_by_id("id_password").send_keys("ucl393939")
 		driver.find_element_by_css_selector("input[type=submit]").click()
 		driver.find_element_by_xpath("//a[@href='/logbook/add/']").click()
@@ -61,5 +64,47 @@ class Testadd(LiveServerTestCase):
 		driver.find_element_by_css_selector("button[type=submit]").click()
 		time.sleep(3)
 
+
+
+class Testsearch(LiveServerTestCase):
+	def setUp(self):
+		self.driver = webdriver.Chrome('/Users/linying/Downloads/chromedriver')
+		super(Testsearch, self).setUp()
+	def tearDown(self):
+		self.driver.quit()
+		super(Testsearch, self).tearDown()
+	def test_login(self):
+		print('search')
+		driver = self.driver
+		driver.get('http://127.0.0.1:8000/logbook/')
+		driver.find_element_by_id("id_username").send_keys("temp22@gmail.com")
+		driver.find_element_by_id("id_password").send_keys("ucl393939")
+		driver.find_element_by_css_selector("input[type=submit]").click()
+		time.sleep(1)
+		driver.find_element_by_xpath("//a[@href='/logbook/search/']").click()
+		driver.find_element_by_css_selector("input[type=text]").send_keys("temp")
+		driver.find_element_by_css_selector("button[type=submit]").click()
+		time.sleep(3)
+
+
+
+
+class TestLogout(LiveServerTestCase):
+	def setUp(self):
+		self.driver = webdriver.Chrome('/Users/linying/Downloads/chromedriver')
+		super(TestLogout, self).setUp()
+	def tearDown(self):
+		self.driver.quit()
+		super(TestLogout, self).tearDown()
+	def test_login(self):
+		print('logout')
+		driver = self.driver
+		driver.get('http://127.0.0.1:8000/logbook/')
+		driver.find_element_by_id("id_username").send_keys("temp22@gmail.com")
+		driver.find_element_by_id("id_password").send_keys("ucl393939")
+		driver.find_element_by_css_selector("input[type=submit]").click()
+		time.sleep(1)
+		driver.find_element_by_xpath("//a[@href='/accounts/logout/?next=/logbook/']").click()
+		time.sleep(3)
 
 
